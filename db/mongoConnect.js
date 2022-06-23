cb2b0ebab2f51847dd00535e8901d0ed4b407dd4
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2022-06-23 10:31:11 +0300

    make it better

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/Express_1');
+    await mongoose.connect('mongodb+srv://idankzm:idankzm2468@cluster0.purdk.mongodb.net/Express_1?retryWrites=true&w=majority');
     console.log("mongo Connect")
 }
 
