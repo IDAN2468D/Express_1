5859dd0aecef4fedd2b814928f85c9841d32bb1b
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2022-07-10 15:05:45 +0300

    required: false

--- models/foodModel.js
+++ models/foodModel.js
@@ -4,7 +4,7 @@ const Joi = require("joi");
 const foodSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: false
     },
     img: String,
     cal: {
