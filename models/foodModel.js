f0b0b54b290cbcd124872d0d54b30de1aca6872d
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2022-07-11 11:33:35 +0300

    .required()

--- models/foodModel.js
+++ models/foodModel.js
@@ -25,7 +25,7 @@ exports.FoodModel = FoodModel;
 
 exports.validFood = (_bodyData) => {
     const joiSchema = Joi.object({
-        name: Joi.string().min(2).max(99),
+        name: Joi.string().min(2).max(99).required(),
         img: Joi.string().min(2).max(300),
         cal: Joi.number().min(1).max(9999),
         price: Joi.number().min(1).max(9999),
