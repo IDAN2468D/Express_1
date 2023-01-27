221974993e0b6737c8483a4a8765e37837ca3581
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2023-01-27 09:29:51 +0200

    params

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.json({ prods_ar })
 })
-
+// ? category?shop/category=food
 router.get("/category", (req, res) => {
     const categoryQ = req.query.category;
     const temp_ar = prods_ar.filter(item => {
@@ -13,5 +13,14 @@ router.get("/category", (req, res) => {
     })
     res.json(temp_ar)
 })
+//shop/category=food
+router.get("/category2/:catName", (req, res) => {
+    const categoryQ = req.params.catName;
+    const temp_ar = prods_ar.filter(item => {
+        return item.cat == categoryQ;
+    })
+    res.json(temp_ar)
+})
+
 
 module.exports = router;
