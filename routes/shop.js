cf68b7d865f7d878890a14ac8ca56554090fe9ad
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2023-01-27 10:35:54 +0200

    QueryId

--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,7 @@ router.get("/category", (req, res) => {
     })
     res.json(temp_ar)
 })
-//shop/category=food
+//shop/category2/foods
 router.get("/category2/:catName", (req, res) => {
     const categoryQ = req.params.catName;
     const temp_ar = prods_ar.filter(item => {
@@ -22,5 +22,20 @@ router.get("/category2/:catName", (req, res) => {
     res.json(temp_ar)
 })
 
+//?min=8
+router.get("/query", (req, res) => {
+    let id = req.params.id
+    let prod = prods_ar.find(item => item.id == id);
+    res.json(prod);
+})
+
+// QueryId
+router.get("/shingle/:id", (req, res) => {
+    const categoryQ = req.params.catName;
+    const temp_ar = prods_ar.filter(item => {
+        return item.cat == categoryQ;
+    })
+    res.json(temp_ar)
+})
 
 module.exports = router;
